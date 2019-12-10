a8362755764403ba5aa95b7f5d3e8133aa32aeb9
danielpeng95 <danielpeng95@yahoo.com>
2019-12-10 13:11:33 -0500

    adding payment return test

---
+++ Package 2/TC_PaymentReturnsTest.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class PaymentReturnsTest(unittest.TestCase):
+    def test_paymentReturnsTest(self):
+        print("This is Payment Returns by bank test")
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
