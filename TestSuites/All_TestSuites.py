1415df4f8614d22a4fd750dfefd7ec58d9d87101
danielpeng95 <danielpeng95@yahoo.com>
2019-12-10 13:21:59 -0500

    add All_TestSuites code

--- TestSuites/All_TestSuites.py
+++ TestSuites/All_TestSuites.py
@@ -1,7 +1,17 @@
+#Creating and Running Test Suites | Batch Testing
 import unittest
 from Package1.TC_LoginTest import LoginTest
 from Package1.TC_SignupTest import SignUpTest
-
-from Package2.TC_PaymentReturnsTest import PaymentReturnsTest
 from Package2.TC_PaymentTest import PaymentTest
+from Package2.TC_PaymentReturnsTest import PaymentReturnsTest
 
+#Get all tests from LogInTest, SignUpTest, PaymentTest, and PaymentReturnsTest
+tc1 = unittest.TestLoader().loadTestsFromTestCase(LoginTest)
+tc2 = unittest.TestLoader().loadTestsFromTestCase(SignUpTest)
+tc3 = unittest.TestLoader().loadTestsFromTestCase(PaymentTest)
+tc4 = unittest.TestLoader().loadTestsFromTestCase(PaymentReturnsTest)
+
+# Creating Test Suites
+sanityTestSuite = unittest.TestSuite([tc1, tc2]) #sanity Test Suite
+
+unittest.TextTestRunner().run(sanityTestSuite)
