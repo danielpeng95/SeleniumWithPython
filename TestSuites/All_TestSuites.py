71f26c498385e949ecd9d7be571d56c7d9a7730d
danielpeng95 <danielpeng95@yahoo.com>
2019-12-10 13:16:00 -0500

    add All_TestSuites code

---
+++ TestSuites/All_TestSuites.py
@@ -0,0 +1,7 @@
+import unittest
+from Package1.TC_LoginTest import LoginTest
+from Package1.TC_SignupTest import SignUpTest
+
+from Package2.TC_PaymentReturnsTest import PaymentReturnsTest
+from Package2.TC_PaymentTest import PaymentTest
+
